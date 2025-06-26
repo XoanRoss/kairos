7653e8294bf47be45fd52b52781963bb23200104
xoanross <xoanross@gmail.com>
2025-06-26 12:41:35 +0200

    refactor(controller): Changed fields validation

--- src/main/java/org/kairos/prices/price/infrastructure/controller/PriceController.java
+++ src/main/java/org/kairos/prices/price/infrastructure/controller/PriceController.java
@@ -2,7 +2,6 @@ package org.kairos.prices.price.infrastructure.controller;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
-import org.kairos.prices.exception.domain.exception.MissingParameterException;
 import org.kairos.prices.price.domain.model.Price;
 import org.kairos.prices.price.domain.port.in.GetPriceUseCase;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +20,9 @@ public class PriceController {
     private final GetPriceUseCase getPriceUseCase;
 
     @GetMapping
-    public ResponseEntity<Price> getPrice(@RequestParam(required = false) @Schema(example = "2020-06-14T21:00:00") LocalDateTime applicationDate,
-                                          @RequestParam(required = false) @Schema(example = "1") Long brandId,
-                                          @RequestParam(required = false) @Schema(example = "35455") Long productId) {
-        if (applicationDate == null) throw new MissingParameterException("Application date");
-        if (brandId == null) throw new MissingParameterException("Brand ID");
-        if (productId == null) throw new MissingParameterException("Product ID");
+    public ResponseEntity<Price> getPrice(@RequestParam @Schema(example = "2020-06-14T21:00:00") LocalDateTime applicationDate,
+                                          @RequestParam @Schema(example = "1") Long brandId,
+                                          @RequestParam @Schema(example = "35455") Long productId) {
         return ResponseEntity.ok(getPriceUseCase.findApplicablePrice(applicationDate, productId, brandId));
     }
 }
