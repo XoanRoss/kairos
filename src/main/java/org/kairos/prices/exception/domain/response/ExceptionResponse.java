3035071c5609dd2fd69f6ed1b07498f43dd316eb
xoanross <xoanross@gmail.com>
2025-06-26 12:40:44 +0200

    chore(ExceptionResponse): Renamed message field to error

--- src/main/java/org/kairos/prices/exception/domain/response/ExceptionResponse.java
+++ src/main/java/org/kairos/prices/exception/domain/response/ExceptionResponse.java
@@ -7,12 +7,12 @@ import java.time.LocalDateTime;
 @Data
 public class ExceptionResponse {
 
-    private String message;
+    private String error;
     private String path;
     private LocalDateTime timestamp;
 
-    public ExceptionResponse(Exception exception, String path){
-        this.message = exception.getMessage();
+    public ExceptionResponse(String error, String path){
+        this.error = error;
         this.path = path;
         this.timestamp = LocalDateTime.now();
     }
