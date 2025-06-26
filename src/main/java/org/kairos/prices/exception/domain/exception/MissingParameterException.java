d28789c106815212032a348103ec4a1bb4cbaf47
xoanross <xoanross@gmail.com>
2025-06-26 12:40:06 +0200

    refactor(exception): Changed exception catching for MissingServletRequestParameterException and MethodArgumentTypeMismatchException

--- src/main/java/org/kairos/prices/exception/domain/exception/MissingParameterException.java
+++
@@ -1,8 +0,0 @@
-package org.kairos.prices.exception.domain.exception;
-
-public class MissingParameterException extends RuntimeException {
-
-    public MissingParameterException(String message) {
-        super("Parameter missing: " + message);
-    }
-}
--- src/main/java/org/kairos/prices/exception/infrastructure/handler/GlobalExceptionHandler.java
+++ src/main/java/org/kairos/prices/exception/infrastructure/handler/GlobalExceptionHandler.java
@@ -2,11 +2,12 @@ package org.kairos.prices.exception.infrastructure.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.kairos.prices.exception.domain.exception.MissingParameterException;
 import org.kairos.prices.exception.domain.exception.ResourceNotFoundException;
 import org.kairos.prices.exception.domain.response.ExceptionResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,14 +17,21 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ExceptionResponse notFound(ResourceNotFoundException ex, HttpServletRequest request) {
         log.warn(ex.getMessage());
-        return new ExceptionResponse(ex, request.getRequestURI());
+        return new ExceptionResponse(ex.getMessage(), request.getRequestURI());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MissingParameterException.class)
-    public ExceptionResponse badRequest(MissingParameterException ex, HttpServletRequest request) {
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ExceptionResponse missingParam(MissingServletRequestParameterException ex, HttpServletRequest request) {
         log.warn(ex.getMessage());
-        return new ExceptionResponse(ex, request.getRequestURI());
+        return new ExceptionResponse("Parameter missing: " + ex.getParameterName(), request.getRequestURI());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ExceptionResponse typeMismatch(MethodArgumentTypeMismatchException ex, HttpServletRequest request) {
+        log.warn(ex.getMessage());
+        return new ExceptionResponse("Parameter not valid: " + ex.getName(), request.getRequestURI());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
